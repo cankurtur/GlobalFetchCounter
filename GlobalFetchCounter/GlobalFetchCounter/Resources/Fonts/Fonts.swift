31b84a59f67f901c6cd626cb82ae894029f20e38
cankurtur-global <cankurtur+global@gmail.com>
2025-02-08 18:48:15 +0100

    add resources

---
+++ GlobalFetchCounter/GlobalFetchCounter/Resources/Fonts/Fonts.swift
@@ -0,0 +1,22 @@
+//
+//  Fonts.swift
+//  GlobalFetchCounter
+//
+//  Created by Can Kurtur on 8.02.2025.
+//
+
+import SwiftUI
+
+extension Font {
+    static var primaryTitle: Font {
+        .system(size: 17, weight: .medium)
+    }
+    
+    static var primarySubtitle: Font {
+        .system(size: 13, weight: .regular)
+    }
+    
+    static var primaryButton: Font {
+        .system(size: 22, weight: .bold)
+    }
+}
