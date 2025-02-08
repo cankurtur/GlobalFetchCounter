96b3d17584c5ec271c01f854864020123c944b05
cankurtur-global <cankurtur+global@gmail.com>
2025-02-08 22:18:59 +0100

    create config plist

---
+++ GlobalFetchCounter/GlobalFetchCounter/Utils/Config/Config.swift
@@ -0,0 +1,26 @@
+//
+//  Config.swift
+//  GlobalFetchCounter
+//
+//  Created by Can Kurtur on 8.02.2025.
+//
+
+import Foundation
+
+final class Config: NSObject {
+    static let shared = Config()
+    
+    var configs: NSDictionary!
+    
+    override private init() {
+        let currentConfiguration = Bundle.app.object(forInfoDictionaryKey: "Config")!
+        let path = Bundle.main.path(forResource: "Config", ofType: "plist")!
+        configs = (NSDictionary(contentsOfFile: path)!.object(forKey: currentConfiguration) as! NSDictionary)
+    }
+}
+
+extension Config {
+    var baseUrl: String {
+        return configs.object(forKey: "baseUrl") as! String
+    }
+}
