9f6bbcd81dc9ffc5aae4d9b1fc1ed7a3dcef2f05
cankurtur-global <cankurtur+global@gmail.com>
2025-02-09 01:24:11 +0100

    write unit test for alert manager

--- GlobalFetchCounter/GlobalFetchCounter/Utils/AlertManager/AlertManagerProtocol.swift
+++ GlobalFetchCounter/GlobalFetchCounter/Utils/AlertManager/AlertManagerProtocol.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 protocol AlertManagerProtocol: ObservableObject {
-    var isShowing: Bool { get }
-    var alertTitle: String { get }
-    var alertMessage: String { get }
+    var isShowing: Bool { get set }
+    var alertTitle: String { get set }
+    var alertMessage: String { get set }
     
     func presentAlert(title: String, message: String)
     func dismissAlert()
