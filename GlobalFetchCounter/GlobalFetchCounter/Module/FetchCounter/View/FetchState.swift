1456b7810d6092a0fb5e030cefdd00fceac95dd0
cankurtur-global <cankurtur+global@gmail.com>
2025-02-11 00:02:30 +0100

    fix fetch state and base url

--- GlobalFetchCounter/GlobalFetchCounter/Module/FetchCounter/View/FetchState.swift
+++ GlobalFetchCounter/GlobalFetchCounter/Module/FetchCounter/View/FetchState.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 /// Enum representing the state of a fetch operation when clicking on fetch button..
-enum FetchState {
+enum FetchState<T> {
     /// Initial state before the fetch starts.
     case initial
     /// The fetch is in progress (loading state).
     case loading
     /// An error occurred during the fetch, includes an `message` for error details.
-    case errorOccuered(message: String)
+    case errorOccured(message: T)
     /// The fetch was successful.
-    case success
+    case success(result: T)
 }
